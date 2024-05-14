7171c92909fe1919541b627558590fab2833048b
eriksgda <erik.ksgda@gmail.com>
2024-05-14 10:16:48 -0300

    update card names

--- src/components/mainCards/cards.tsx
+++ src/components/mainCards/cards.tsx
@@ -1,15 +1,12 @@
 import { NameCard } from './nameCard'
 import AlbumFoto from '/Users/Pichau/www/programs/projects/spotify-clone/src/assets/album.jpg'
 
-const name = ['pinto', 'blerght', 'amixade', 'sexo', 'sexo 2']
-
-
 export function Cards(){
   return(
   <a href='' className='flex flex-col gap-2 bg-black/10 p-3 rounded-md hover:bg-white/5 transition-colors'>
     <img src={AlbumFoto} className='w-full' width={80} height={80} alt="Dirt do Alice In Chains" />
-    <NameCard children={name[Math.ceil(Math.random() * 5) - 1]}/>
-    <span className='text-sm text-zinc-400'>Alice In Chains, Leipa, Sol Sistere and more</span>
+    <NameCard children={'Playlist ' + (Math.ceil(Math.random() * 50) + 1).toString()}/>
+    <span className='text-sm text-zinc-400'>Alice In Chains, The Cure, Erasure and more</span>
   </a>
             
   )
